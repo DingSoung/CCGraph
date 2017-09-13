41438d8e7d817bbb50cd24ea156009f3f354c33e
Ding Songwen <DingSoung@gmail.com>
2017-09-13 17:17:34 +0800

    改写成MTView, 升级Xcode

--- MetalExample/MetalExample/Renderer.swift
+++ MetalExample/MetalExample/Renderer.swift
@@ -4,39 +4,29 @@
 import UIKit
 
 class Renderer: NSObject {
+    weak var metalLayer: CAMetalLayer! = nil
+
     var device: MTLDevice! = nil
-    var metalLayer: CAMetalLayer! = nil
-    var vertexBuffer: MTLBuffer! = nil
-    var pipelineState: MTLRenderPipelineState! = nil
     var commandQueue: MTLCommandQueue! = nil
+    var pipelineState: MTLRenderPipelineState! = nil
+    // MTLDepthStencilState
+    // MTLSamplerState
+    // MTLTexture
+
+    var vertexBuffer: MTLBuffer! = nil
     var timer: CADisplayLink! = nil
     
     init?(layer: CAMetalLayer) {
-        self.device = MTLCreateSystemDefaultDevice()
+        // Ask for the default Metal device; this represents our GPU.
+        if let defaultDevice = MTLCreateSystemDefaultDevice() {
+            device = defaultDevice
+        } else {
+            print("Metal is not supported")
+            return nil
+        }
+        self.commandQueue = self.device.makeCommandQueue()
 
-        self.metalLayer = layer;
-        self.metalLayer.device = device
-        self.metalLayer.pixelFormat = .bgra8Unorm
-        self.metalLayer.framebufferOnly = true
-
-
-
-        //open var device: MTLDevice?
-        //open var pixelFormat: MTLPixelFormat
-
-        //open var framebufferOnly: Boo
-        //open var drawableSize: CGSize
-        //open func nextDrawable() -> CAMetalDrawable?
-        //open var presentsWithTransaction: Bool
-
-        let vertexData:[Float] = [
-            0.0, 0.5, 0.0,
-            -1.0, -0.5, 0.0,
-            1.0, -0.5, 0.0]
-        let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
-        self.vertexBuffer = self.device.makeBuffer(bytes: vertexData, length: dataSize, options: MTLResourceOptions())
-
-        let defaultLibrary = device.newDefaultLibrary()
+        let defaultLibrary = device.makeDefaultLibrary()
         let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
         pipelineStateDescriptor.vertexFunction = defaultLibrary?.makeFunction(name: "basic_vertex")
         pipelineStateDescriptor.fragmentFunction = defaultLibrary?.makeFunction(name: "basic_fragment")
@@ -47,10 +37,22 @@ class Renderer: NSObject {
             print("Failed to create pipeline state, error \(error)")
             return nil
         }
-
-        self.commandQueue = self.device.makeCommandQueue()
-
+        // MTLDepthStencilState
+        // MTLSamplerState
+        // MTLTexture
         super.init()
+
+        self.metalLayer = layer;
+        self.metalLayer.device = device
+        self.metalLayer.pixelFormat = .bgra8Unorm
+        self.metalLayer.framebufferOnly = true
+
+        let vertexData:[Float] = [
+            0.0, 0.5, 0.0,
+            -1.0, -0.5, 0.0,
+            1.0, -0.5, 0.0]
+        let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
+        self.vertexBuffer = self.device.makeBuffer(bytes: vertexData, length: dataSize, options: MTLResourceOptions())
 
         self.timer = CADisplayLink(target: self, selector: #selector(Renderer.gameloop))
         self.timer.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
@@ -69,17 +71,17 @@ class Renderer: NSObject {
         renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
 
         let commandBuffer = self.commandQueue.makeCommandBuffer()
-        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
-        renderEncoder.setRenderPipelineState(self.pipelineState)
-        renderEncoder.setVertexBuffer(self.vertexBuffer, offset: 0, at: 0)
-        renderEncoder.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 3, instanceCount: 1)
-        renderEncoder.endEncoding()
+        let renderEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        renderEncoder?.setRenderPipelineState(self.pipelineState)
+        renderEncoder?.setVertexBuffer(self.vertexBuffer, offset: 0, index: 0)
+        renderEncoder?.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+        renderEncoder?.endEncoding()
 
-        commandBuffer.present(drawable)
-        commandBuffer.commit()
+        commandBuffer?.present(drawable)
+        commandBuffer?.commit()
     }
 
-    func gameloop() {
+    @objc func gameloop() {
         autoreleasepool {
             self.render()
         }
